7784434b2ad4e40be25889ce844bd4d6c184a4e3
Lukas R <lukas.rustemeyer@fluppisoft.com>
2025-12-07 21:27:52 +0100

    - Make day 6 solution a bit nicer

--- cpp/AdventOfCode.cpp
+++ cpp/AdventOfCode.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[])
 {
     aoc::assert_result(std::make_tuple(4277556ull, 3263827ull), aoc::day6::solve(true));
-    aoc::assert_result(std::make_tuple(4878670269096ull, 0ull), aoc::day6::solve(false));
+    aoc::assert_result(std::make_tuple(4878670269096ull, 8674740488592ull), aoc::day6::solve(false));
     return 0;
 }
--- cpp/y2025/day6.h
+++ cpp/y2025/day6.h
@@ -51,34 +51,36 @@ namespace aoc::day6
             // Get all operators from the last row
             const auto ops = ops_line | std::views::filter([](const auto c) { return !std::isspace(c); }) | std::ranges::to<std::vector>();
 
+            // 1D matrix of the numbers in all rows and columns
             const auto numbers = number_lines | std::views::transform([](const auto& line)
             {
                 return std::ranges::subrange(line.begin(), line.end())
                     | std::views::split(' ')
                     | std::views::filter([](auto&& s) { return s.begin() != s.end(); })
-                    | std::views::transform([](auto&& s) { return std::stoull(std::string(s.begin(), s.end())); })
-                    | std::ranges::to<std::vector>();
-            }) | std::ranges::to<std::vector>();
+                    | std::views::transform([](auto&& s) { return std::stoull(std::string(s.begin(), s.end())); });
+            }) | std::views::join | std::ranges::to<std::vector>();
 
             const auto num_columns = ops.size();
 
-            auto column_results = std::views::iota(0ull, num_columns) | std::views::transform([&](const auto column)
+            // Calculate the results for all columns
+            const auto column_results = std::views::iota(0ull, num_columns) | std::views::transform([&](const auto column)
             {
                 auto columns = std::views::iota(0ull, number_lines.size()) | std::views::transform([&](const auto line_idx)
                 {
-                    return numbers[line_idx][column];
+                    const auto index = column + line_idx * num_columns;
+                    return numbers[index];
                 });
 
                 return do_math(columns, ops[column]);
             });
 
+            // Accumulate results
             result_p1 = std::accumulate(column_results.begin(), column_results.end(), 0ull);
         }
 
         // Calculate part 2
         auto result_p2 = 0ull;
         {
-            auto numbers = std::vector<unsigned long long>();
             for (auto op_it = ops_line.begin(); op_it < ops_line.end();)
             {
                 // Find the next operator
@@ -89,20 +91,20 @@ namespace aoc::day6
 
                 const auto num_columns = static_cast<unsigned long long>(next_op_it == ops_line.end() ? next_op_it - op_it : next_op_it - op_it - 1);
 
-                numbers.clear();
+                // Generate numbers from columns
                 const auto column_offset = op_it - ops_line.begin();
-                for (auto column_idx = 0ull; column_idx < num_columns; ++column_idx)
+                const auto numbers = std::views::iota(0ull, num_columns) | std::views::transform([&](const auto column_idx)
                 {
                     // Get a vertical slice from the lines and strip spaces
-                    auto number_range = number_lines | std::views::transform([&](const auto& s) { return s[column_idx + column_offset]; }) | std::views::filter([](const char c)
+                    auto chars = number_lines | std::views::transform([&](const auto& s) { return s[column_idx + column_offset]; }) | std::views::filter([](const char c)
                     {
                         return !std::isspace(c);
                     });
 
                     // Convert to a string and then an integer
-                    const auto number_str = std::string(number_range.begin(), number_range.end());
-                    numbers.push_back(std::stoull(number_str));
-                }
+                    const auto number_str = std::string(chars.begin(), chars.end());
+                    return std::stoull(number_str);
+                });
 
                 result_p2 += do_math(numbers, *op_it);
 
